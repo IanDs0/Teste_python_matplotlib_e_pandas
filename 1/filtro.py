de27001553bdbd83a76513d20c62dd51b246957b
IanDs0 <ianhalominecraft@gmail.com>
2021-12-01 15:56:25 -0300

    1

---
+++ 1/filtro.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+lol = pd.read_csv('./2021_LoL_esports.csv')
+
+#Liga/Time/Resltado/
+lol_partidas = lol.groupby(['team','league','result'])['result'].count()/6
+
+print(lol_partidas.head())
+
+#plt.bar(lol_partidas['team'],lol_partidas['result'])
+lol_partidas.plot(kind = 'bar')
