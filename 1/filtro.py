41254147a6256715f371aca63923e812ca5744cd
IanDs0 <ianhalominecraft@gmail.com>
2021-12-03 23:37:31 -0300

    Só o d pizza sem os nomes

--- 1/filtro.py
+++ 1/filtro.py
@@ -2,14 +2,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #linux
-#lol = pd.read_csv('./2021_LoL_esports.csv')
+lol = pd.read_csv('./1/LOL2021_LoL_esports.csv')
 #windows
-lol = pd.read_csv('1\LOL2021_LoL_esports.csv')
+#lol = pd.read_csv('1\LOL2021_LoL_esports.csv')
 
-#Liga/Time/Resltado/
-lol_partidas = lol.groupby(['team','league','result'])['result'].count()/6
+#Agrupado - Liga/Time/Resltado/
+lol_partidas = lol.groupby(['league','team','result'])['result'].count()
 
 print(lol_partidas.head())
 
-#plt.bar(lol_partidas['team'],lol_partidas['result'])
+#Numero da partida que o Champ apareceu
+lol['Champs_num'] = lol['champion'].map({'Akali': 0, 'Lee Sin': 1, 'Irelia': 2})
+
+lol_champs = lol.groupby('champion')['champion'].count()
+
+print(lol_champs.head())
+
+plt.pie(lol_champs, labels=lol_champs)
+plt.show()
+
+plt.scatter(lol['league'],lol['team'])
+plt.show()
+
 lol_partidas.plot(kind = 'bar')
+plt.show()
